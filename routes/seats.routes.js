9cc0860198e5240f10aea2d06f64199352742c34
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-29 12:41:38 +0200

    add endpoints into new routes

---
+++ routes/seats.routes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const uuidv4 = require('uuid/v4');
+const db = require('../db/db');
+
+
+router.route('/seats').get((req, res) => {
+    res.json(db.seats);
+});
+
+router.route('/seats/:id').get((req, res) => {
+    res.json(db.seats.filter(data => data.id == req.params.id));
+});
+
+router.route('/seats').post((req, res) => {
+    const newData = {
+        id: uuidv4(),
+        day: req.body.day,
+        seat: req.body.seat,
+        client: req.body.client,
+        email: req.body.email,
+    };
+    db.seats.push(newData);
+    res.json({ message: 'OK' });
+});
+
+router.route('/seats/:id').put((req, res) => {
+    db.seats = db.seats.map(data => data.id == req.params.id? {...data, day: req.body.day, seat: req.body.seat, client: req.body.client, email: req.body.email } : data);
+    res.json({ message: 'OK' });
+});
+
+router.route('/seats/:id').delete((req, res) => {
+    db.seats = db.seats.filter(data => data.id != req.params.id);
+    res.json({ message: 'OK' });
+});
+
+
+module.exports = router; 

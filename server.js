21315e9b579471f0776746c9122314d66c786649
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-29 15:00:58 +0200

    prepare to publish on Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const path = require('path');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
@@ -14,9 +15,15 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.use(express.static(path.join(__dirname + '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 })
-app.listen(8000, () => {
+app.listen( process.env.PORT || 8000, () => {
   console.log('Server is running on port 8000');
 });

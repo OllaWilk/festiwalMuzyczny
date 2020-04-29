77c421ee9bb0a5162a564a70463fa7ef3118cd67
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2020-04-29 16:59:36 +0200

    add seatUpdated emiter to server

--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -23,6 +23,7 @@ router.route('/seats').post((req, res) => {
     if (!db.seats.some(data => data.day === req.body.day && data.seat === req.body.seat)) {
         db.seats.push(newData);
         res.json({message:'ok'});
+        io.emit('seatsUpdated', db.seats);
     } else {
         res.status(409).json({message: "The slot is already taken"})
     };
--- server.js
+++ server.js
@@ -9,8 +9,13 @@ const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
 app.use(cors());
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
